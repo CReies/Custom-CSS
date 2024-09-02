80f0bd273b3e5c1d7590f9cffb942afe8c1bde22
CReies <ccreyesmolina@gmail.com>
2024-09-02 18:33:22 -0500

    Remove unnecessary script

--- campus-dark.js
+++
@@ -1,5 +0,0 @@
-let cssLink = document.createElement("link");
-cssLink.href = "https://cdn.jsdelivr.net/gh/CReies/Custom-CSS@main/campus-dark.css";
-cssLink.rel = "stylesheet";
-cssLink.type = "text/css";
-frames['iframe1'].document.head.appendChild(cssLink);
